dbf1fc3ff2ea30d2291abf64b33e739b9b1b1407
wagcostalonga <costalongawagner@gmail.com>
2020-03-30 18:34:24 +0100

    third commit

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,5 +1,5 @@
+const generateUniqueId = require('../util/generateUniqueId');
 const connection = require('../database/connection');
-const crypto = require('crypto');
 
 module.exports = {
   /**Listagem de ONG's */
@@ -13,7 +13,7 @@ module.exports = {
   async create(request, response) {
     const { name, email, whatsapp, city, uf } = request.body;
 
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = generateUniqueId();
 
     await connection('ongs').insert({
       id,
